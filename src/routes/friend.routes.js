2853f1a18a3015f3df7352eb8d3b3cef60194afa
CodeByPradip <pradipkhadka024@gmail.com>
2026-07-02 15:05:39 +0545

    feat: update friend routes and middleware

--- src/routes/friend.routes.js
+++ src/routes/friend.routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const { authUserMiddleware } = require("../middlewares/auth.middleware");
 const router = express.Router();
-const { sendRequest ,acceptRequest,cancleRequest} = require("../controllers/friendRequist.controller");
+const { sendRequest ,acceptRequest,cancleRequest,rejectRequest} = require("../controllers/friendRequist.controller");
 
 // FRIEND REQUEST    =- POST /api/friends/request
-// ACCEPT FRIENDS REQUEST PATCH /api/friends/request/:requestId/accept
-// CANCLE FRIEND REQUEST AND DELETE FRIEND COLLECTION FOR SENDER DELETE api/friends/request/:requestId/cancle
-// DELETE FRIEND-REQUEST-DOCUMENT by receveir  DELETE api/friends/request/:requestId
+// ACCEPT  REQUEST =-   PATCH /api/friends/request/:requestId/accept  <== isme friend (collection) model ka id pass karna hai
+// CANCLE  REQUEST AND DELETE FRIEND COLLECTION FOR SENDER   =- DELETE api/friends/request/:requestId/cancle
+// DELETE FRIEND-REQUEST-DOCUMENT by Receiver    =- DELETE  api/friends/request/:requestId/reject
 
 
 
 router.post("/request", authUserMiddleware, sendRequest);
 router.patch("/request/:requestId/accept", authUserMiddleware, acceptRequest);
-router.delete("/request/:requestId/cancle",authUserMiddleware,cancleRequest)
+router.delete("/request/:requestId/cancle",authUserMiddleware,cancleRequest);
+router.delete("/request/:requestId/reject",authUserMiddleware,rejectRequest);
 
 module.exports = router;
